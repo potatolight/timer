b5c8c585591440ca9338d7ee82e5e726b5ab0502
potatolight <shuguangbai@gmail.com>
2020-10-12 22:58:17 -0700

    add if key equal to b and if key is 0 to 9

--- timer2.js
+++ timer2.js
@@ -1,1 +1,33 @@
-let arr = process.argv.slice(2);
+
+const stdin = process.stdin;
+
+stdin.setRawMode(true);
+stdin.setEncoding('utf8');
+
+
+stdin.on('data', (key) => {
+  // console.log(key);
+
+  if (key === 'b') {
+    console.log('Sounds is here');
+    // process.stdout.write('\x07');
+  }
+
+  if (key <= 9 && key >= 1) {
+
+    process.stdout.write(`Setting timer for ${key} seconds...`);
+
+    setTimeout(() => {
+      console.log('Sounds');
+      // process.stdout.write('\x07');
+    }, key * 1000);
+  }
+
+  if (key === '\u0003') {
+    process.stdout.write('Thanks for using me, ciao!\n');
+    process.exit();
+  }
+});
+
+
+
